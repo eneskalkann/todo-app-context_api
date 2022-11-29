27a0ffd5115bd8beeada1e711b1666bfa474919d
eneskalkann <eneskalkan36e@gmail.com>
2022-11-29 21:33:36 +0300

    adding-some-animation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import Todo from "./Components/Todo";
 import TodoList from "./Components/TodoList";
 import GlobalContext from "./Context/GlobalContext";
+import { motion } from "framer-motion";
+
+const variants ={
+  hidden : {opacity:0},
+  visible:{opacity:1},
+  transition : {duration:1}
+}
 
 function App() {
   return (
-    <div className="bg-tealBlue min-h-screen max-w-screen flex items-center justify-center bg-contain">
+    <motion.div variants={variants} initial="hidden" animate="visible" className="bg-tealBlue min-h-screen max-w-screen flex items-center justify-center bg-contain">
       <div className="flex-col text-center relative w-screen">
       <header>
         <h1 className='text-eggshell text-3xl font-bold'>Todo App</h1>
@@ -14,7 +21,7 @@ function App() {
         <Todo/>
       </GlobalContext>
       </div>
-    </div>
+    </motion.div>
   );
 }
 
--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,24 +1,32 @@
 import React from 'react'
 import { useTodoContext } from '../Context/GlobalContext'
 import { AiTwotoneDelete } from 'react-icons/ai'
+import { motion , AnimatePresence} from 'framer-motion';
+
+const variants ={
+    hidden : {opacity:0},
+    visible:{opacity:1},
+    transition : {duration:1}
+}
 
 function Todo() {
 
     const { todo, deleteTodo } = useTodoContext();
 
     return (
-        <div>
+        <AnimatePresence>
             {
                 todo.map((todo, index) => (
-                    <div className='px-10 py-1 mt-2 flex items-center justify-center break-all'>
-                        <h3 key={index} className='font-medium bg-eggshell px-4 py-3 w-72 md:w-4/12 break-words bg-white flex items-center justify-center rounded-lg text-metalicSunburst'>
+                    
+                    <ul className='px-10 py-1 mt-2 flex items-center justify-center break-all'>
+                        <motion.li variants={variants} initial="hidden" animate="visible" exit="hidden" key={index} className='font-medium bg-eggshell px-4 py-3 w-72 md:w-4/12 break-words bg-white flex items-center justify-center rounded-lg text-metalicSunburst'>
                             {todo}
-                        </h3>
+                        </motion.li>
                         <button onClick={() => deleteTodo(index)} ><AiTwotoneDelete className="text-maizeCrayola ml-3 md:ml-7 text-xl" /></button>
-                    </div>
+                    </ul>
                 ))
             }
-        </div>
+        </AnimatePresence>
     )
 }
 
