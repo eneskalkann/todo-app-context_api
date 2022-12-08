e900ef2a62c4a8140901fcbdb0b16ed25aa4b7d5
eneskalkann <eneskalkan36e@gmail.com>
2022-12-08 23:06:43 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Todo from "./Components/Todo";
 import TodoList from "./Components/TodoList";
+import TodoLength from "./Components/TodoLength";
 import GlobalContext from "./Context/GlobalContext";
 import { motion } from "framer-motion";
 
@@ -14,10 +15,11 @@ function App() {
     <motion.div variants={variants} initial="hidden" animate="visible" className="bg-tealBlue min-h-screen max-w-screen flex items-center justify-center bg-contain">
       <div className="flex-col text-center relative w-screen">
       <header>
-        <h1 className='text-eggshell text-3xl font-bold'>Todo App</h1>
+        <h1 className='text-eggshell text-3xl font-bold mb-4'>Todo App</h1>
       </header>
       <GlobalContext>
         <TodoList/>
+        <TodoLength/>
         <Todo/>
       </GlobalContext>
       </div>
